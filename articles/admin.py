31a3242419b6f322e2a72e99e1c703ef70a0b85c
Munam Mubashir <munam.mubashir@arbisoft.com>
2020-08-16 17:15:51 +0500

    Register profile, tag, comment and article model to Admin panel
    Registered all the models to the admin panel. Created a superuser to use the admin panel

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Article, Tag, Comment
+# Register your models here.
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'created_at')
+    search_fields = ('title', 'body')
+    raw_id_fields = ('author',)
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'article', 'body', 'created_at')
