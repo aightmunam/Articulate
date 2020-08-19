94974e7067a8b39e581ea3e5c71ef867df5bde5f
Munam Mubashir <munam.mubashir@arbisoft.com>
2020-08-19 14:56:50 +0500

    Config postgres as the default database

--- articulate/settings.py
+++ articulate/settings.py
@@ -38,9 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
     'articles',
     'profiles',
-    'taggit'
 ]
 
 MIDDLEWARE = [
@@ -78,9 +78,13 @@ WSGI_APPLICATION = 'articulate.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'articulate',
+        'USER': 'articulate',
+        'PASSWORD': 'articulate',
+         'HOST': 'localhost'
     }
 }
 
