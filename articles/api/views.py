1bd595ed60e46faeb6c9e00e8b4283c8a97ab216
Munam Mubashir <munam.mubashir@arbisoft.com>
2020-09-08 05:13:44 +0500

    Add generic views to provide the view functionality for articles
    This commit reduces the code even further by removing any mixins and instead using the complete generic list and detail views provided by the django rest framework

--- articles/api/views.py
+++ articles/api/views.py
@@ -1,37 +1,15 @@
-from rest_framework import status, generics, mixins
-from rest_framework.exceptions import NotFound
+from rest_framework import generics
 
 from articles.models import Article
 from articles.api.serializers import ArticleSerializer
 
 
-class ArticleList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
+class ArticleList(generics.ListCreateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class ArticleDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class ArticleDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
     lookup_field = 'slug'
-
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
