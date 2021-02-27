1423945fe92d202f14eeb45bc0a3fa9738b483d9
MunamMubashir-arbisoft <munam.mubashir@arbisoft.com>
2021-02-27 16:48:18 +0500

    Make DB host selection dynamic

--- articulate/settings.py
+++ articulate/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USERNAME'),
         'PASSWORD': os.environ.get('DB_PASS'),
-        'HOST': 'localhost'
+        'HOST': os.environ.get('HOST')
     }
 }
 
