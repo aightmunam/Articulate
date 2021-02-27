9da27e851a9b5f40f6c350a4d4f504763328e4cd
MunamMubashir-arbisoft <munam.mubashir@arbisoft.com>
2021-02-27 18:21:01 +0500

    Clean static data and settings

--- articulate/settings.py
+++ articulate/settings.py
@@ -140,7 +140,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfies')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
